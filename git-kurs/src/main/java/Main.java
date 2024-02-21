138a63c1e8a276aee498d351e84ea6860ec67b98
zozola <zozola@github.com>
2024-02-21 13:22:10 +0100

    Hello new user

--- git-kurs/src/main/java/Main.java
+++ git-kurs/src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main (String[] args) {
+<<<<<<< HEAD
         System.out.println("hello new changes!");
+=======
+        System.out.println("hello new user!");
+        
+    }
+    private void displayText() {
+         System.out.println("zmiana w drugim repo");
+
+>>>>>>> 2dca29f (Hello new user)
     }
 }
